93bf96b456fbf96ec86c6f356c2b88b6fb7c9e49
Madhavam Shahi <madhavampshahi@gmail.com>
2025-03-03 15:37:45 -0600

    pushing watchers

--- hogwarts-shopping-cart/src/App.vue
+++ hogwarts-shopping-cart/src/App.vue
@@ -1,66 +1,56 @@
 <template>
   <h1>{{ message }}</h1>
-  <div>
-    <form @submit.prevent="register">
-      <div>
-        <label for="email">Email:</label>
-        <input
-          id="email"
-          v-model="email"
-          type="email"
-          placeholder="Enter your email"
-        />
-      </div>
-      <div>
-        <label for="password">Password:</label>
-        <input
-          id="password"
-          v-model="password"
-          type="password"
-          placeholder="Create a password"
-        />
-      </div>
-      <button type="submit" :disabled="!isFormValid">Register</button>
-    </form>
-  </div>
-  <hr />
+
   <div class="card">
+    <h1>Scenario 1: Watch a "ref(primitive value)"</h1>
+    <h2>Number: {{ number }}</h2>
+    <button @click="number++">Increment number by 1</button>
+  </div>
+
+  <div class="card">
+    <h1>Scenario 2: Watch a property in "ref(object)"</h1>
     <h2>Name: {{ wizard1.name }}</h2>
     <h2>Wand: {{ wizard1.wand }}</h2>
-    <h2>Age: {{ wizard1.age }}</h2>
     <button @click="wizard1.name = wizard1.name.toUpperCase()">
       Change name to upper case
     </button>
+    <button @click="changeWizard1Wand">Change wand</button>
     <button @click="wizard1.wand.core = 'Unicorn hair'">
       Change wand core
     </button>
-    <button @click="wizard1.age = 20">Change age</button>
+  </div>
+
+  <div class="card">
+    <h1>Scenario 3: Watch a "ref(object)"</h1>
+    <h2>Name: {{ wizard2.name }}</h2>
+    <h2>Wand: {{ wizard2.wand }}</h2>
+    <button @click="wizard2.name = wizard2.name.toUpperCase()">
+      Change name to upper case
+    </button>
+    <button @click="wizard2.wand.core = 'Phoenix feather'">
+      Change wand core
+    </button>
+    <button @click="changeWizard">Change wizard</button>
   </div>
 </template>
 
 <script setup>
-import { ref, watchEffect } from 'vue'
-let message = ref('Hello, watchEffect!')
-const email = ref('')
-const password = ref('')
-const isFormValid = ref(false)
+import { ref, watch } from 'vue'
 
-// watch([email, password], () => {
-//   const hasEmail = email.value.length > 0
-//   const hasPassword = password.value.length > 0
-//   isFormValid.value = hasEmail && hasPassword
-// })
+let message = ref('Hello, Watchers!')
 
-watchEffect(() => {
-  console.log('watchEffect')
-  const hasEmail = email.value.length > 0
-  const hasPassword = password.value.length > 0
-  isFormValid.value = hasEmail && hasPassword
+let number = ref(1)
+
+const stopWatch = watch(number, (newValue, oldValue) => {
+  console.log(
+    'Watch a ref(primitive value): number changes',
+    newValue,
+    oldValue
+  )
+  if (newValue >= 5) {
+    stopWatch()
+  }
 })
-
-const register = () => {
-  alert('Registration successful!')
-}
 
 let wizard1 = ref({
   id: 1001,
@@ -73,16 +63,70 @@ let wizard1 = ref({
   }
 })
 
-watchEffect(() => {
-  console.log(wizard1.value.name, wizard1.value.wand.core)
+function changeWizard1Wand() {
+  wizard1.value.wand = {
+    core: 'Dragon heartstring',
+    wood: 'Vine'
+  }
+}
+
+watch(
+  () => wizard1.value.name,
+  (newValue, oldValue) => {
+    console.log(
+      'Watch a property in a ref(object): wizard1 name changes',
+      newValue,
+      oldValue
+    )
+  }
+)
+
+watch(
+  () => wizard1.value.wand,
+  (newValue, oldValue) => {
+    console.log(
+      'Watch a property in a ref(object): wizard1 wand changes',
+      newValue,
+      oldValue
+    )
+  },
+  { deep: true }
+)
+
+let wizard2 = ref({
+  id: 1003,
+  name: 'Ron Weasley',
+  house: 'Gryffindor',
+  age: 17,
+  wand: {
+    core: 'Unicorn hair',
+    wood: 'Willow'
+  }
 })
 
-watchEffect(() => {
-  console.log(wizard1.value)
-})
+function changeWizard() {
+  wizard2.value = {
+    id: 1002,
+    name: 'Hermione Granger',
+    house: 'Gryffindor',
+    age: 17,
+    wand: {
+      core: 'Dragon heartstring',
+      wood: 'Vine'
+    }
+  }
+}
 
+watch(
+  wizard2,
+  (newValue, oldValue) => {
+    console.log('Watch a ref(object): wizard2 changes', newValue, oldValue)
+  },
+  { deep: true }
+)
 
 </script>
+
 <style scoped>
 .card {
   background-color: purple;
