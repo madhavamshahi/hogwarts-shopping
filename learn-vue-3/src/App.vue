4bb947c14a9ce7af0243ae57a59ccb4c0201931b
Madhavam Shahi <madhavampshahi@gmail.com>
2025-03-10 22:01:08 -0500

    provide inject vue

--- learn-vue-3/src/App.vue
+++ learn-vue-3/src/App.vue
@@ -1,16 +1,29 @@
 <template>
-  <AlertBox></AlertBox>
-  <AlertBox>Invalid Username or password. Please try again!</AlertBox>
-  <AlertBox>
-    <h1>The request timed out. Please refresh the page and try again.</h1>
-  </AlertBox>
+  <h1>Message: {{ message }}</h1>
+  <div style="background-color: orange; padding-left: 10px;">
+    <ComponentA />
+  </div>
+  <button @click="count = count + 2">Add to count</button>
+  
 </template>
 
 <script setup>
-import AlertBox from './AlertBox.vue';
+import { provide, readonly, ref } from 'vue';
+import ComponentA from './ComponentA.vue';
+
+let message = ref('Hello, Provide/inject!')
+
+let count = ref(0)
+
+function updateMessage() {
+  message.value = 'Hello, Provide/inject! Updated.'
+}
+
+provide('msg', {message, updateMessage})
+provide('read-only-count', readonly(count))
 
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
